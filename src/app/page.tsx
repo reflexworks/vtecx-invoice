b0fee31ffbdfbc9e816a0ab9563a3727b699c4b7
https://github.com/hiroki-morishita <morishita@virtual-tech.net>
2026-06-19 16:44:42 +0900

    ログイン画面に遷移させる対応

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,23 +1,7 @@
-'use client'
-
-import Loader from '@/components/loader'
-import { Typography } from '@mui/material'
-import Grid from '@mui/material/Grid2'
+import { redirect } from 'next/navigation'
 
 const HomePage = () => {
-  return (
-    <Loader>
-      <div style={{ marginTop: '100px' }}>
-        <Grid container direction="column" justifyContent="center" alignItems="center" spacing={4}>
-          <Grid size={{ xs: 12, md: 5 }} textAlign={'center'}>
-            <Typography variant="h2" gutterBottom>
-              Hello vte.cx!!
-            </Typography>
-          </Grid>
-        </Grid>
-      </div>
-    </Loader>
-  )
+  redirect('/login')
 }
 
 export default HomePage
